ba9223001c44050d2bd924f0f6b8edb963b13c7a
jtrull101 <33765387+jtrull101@users.noreply.github.com>
2023-10-06 12:39:29 -0400

    remove models less than 85% success

--- model_training.py
+++ model_training.py
@@ -80,31 +80,35 @@ def load_data(N):
     
 class callback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
-        if logs.get('loss') < 0.3:
+        if logs.get('acc') >= 0.995:
             self.model.stop_training = True
     
 def create_model(num_classes):
-    tf.random.set_seed(1234)
-
-    callbacks = callback()
+    tf.random.set_seed(1234) 
 
     # Create tensorflow model
     model = Sequential(
         [
+            # Convolution and Pooling 3 times before flattening to reduce total number of pixels passed to last Dense layer
             Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),  # relu - ReLU(x)=max(0,x)
-            MaxPooling2D(),
-            Dropout(0.15), 
+            MaxPooling2D(2,2),
+            # Dropout(0.15), 
+            Conv2D(32, (3,3), activation='relu'),
+            MaxPooling2D(2,2), 
+            # Dropout(0.25),                                                  # select a percentage of outputs at random and set to 0, helps regularize
             Conv2D(64, (3,3), activation='relu'),
-            MaxPooling2D(), 
-            Dropout(0.25),                                                  # select a percentage of outputs at random and set to 0, helps regularize
+            MaxPooling2D(2,2),
+            Conv2D(64, (3,3), activation='relu'),
+            MaxPooling2D(2,2),
             Flatten(),                                                      # take 4D array, turn into vector
             Dense(128, activation='relu'),
-            Dropout(0.5),
+            # Dropout(0.5),
             Dense(num_classes, activation='softmax')
         ]
     )
     
-    # print(model.summary())
+    print(model.summary())
+
     return model
     
     
@@ -112,8 +116,8 @@ def main():
     os.chdir("/home/jtrull/python/alzheimers")
     
     f = open('histories.txt', 'a')
-    f.write(f"test run: {datetime.now().strftime('%m/%d/%Y, %H:%M:%S')}\n")
-    f.write(f"accuracy percent,percent of data,batch size,history.params,history.history['acc'],history.history['loss'],elapsed_time\n")
+    f.write(f"\ntest run: {datetime.now().strftime('%m/%d/%Y, %H:%M:%S')}\n")
+    f.write(f"accuracy percent,percent of data,batch size,learning_rate,history.params,history.history['acc'],history.history['loss'],elapsed_time\n")
                     
     f.close()
     
@@ -122,9 +126,9 @@ def main():
     #       features are just intensity values of pixels
 
     data_subets =   [ 0.5, 0.75, 0.99 ]                                             # may need to downsample images before using more data
-    epochs =        [ 5, 10, 25, 50, 75, 100 ]                                      # 'random' scaling numbers
-    batch_sizes =   [ 8, 16, 32, 64, 128 ]                                          # powers of 2
-    
+    epochs =        [ 10, 25, 50, 75, 100 ]                                      # 'random' scaling numbers
+    batch_sizes =   [ 32, 64, 128 ]                                          # powers of 2
+    learning_rate = 0.001
     for d in data_subets:
         for e in epochs:
             for b in batch_sizes:
@@ -137,18 +141,20 @@ def main():
                     # Compile model
                     model.compile(
                         loss=tf.keras.losses.categorical_crossentropy,
-                        optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+                        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
                         metrics=['acc']
                     )
                     
                     model.build()
                     print(f"Model params:{model.count_params()}")
 
+                    callbacks = callback()
                     history = model.fit(x_train, y_train,
                             batch_size=b,
                             epochs=e,
                             verbose=1,                                  # type: ignore
-                            validation_data=(x_cv, y_cv))
+                            validation_data=(x_cv, y_cv),
+                            callbacks=[callbacks])
                     loss, acc = model.evaluate(x_test, y_test, verbose=0)   # type: ignore
                     
                     print('Test loss:', loss)
@@ -158,15 +164,20 @@ def main():
                     acc_perc = f'{int(acc*100)}%'
                     data_perc = f'{int(d*100)}%'
                     f = open('histories.txt', 'a')
-                    # write out history
-                    f.write(f"{acc_perc}, {data_perc}, {b}, {history.params}, {history.history['acc']}, {history.history['loss']}, {elapsed_time}\n")
-                    f.close()  
-                    # gc
-                    del history 
-                    if acc >= 0.8:
-                        model.save(f'models/alz_cnn_{e}_es_{b}_bs_{data_perc}_data_{acc_perc}_acc_{loss:.2f}_loss_{elapsed_time}_seconds.keras')
+                    
+                    if acc >= 0.95:
+                        model.save(f'models/alz_cnn_{e}_es_{b}_bs_{learning_rate}_lr_{data_perc}_data_{acc_perc}_acc_{loss:.2f}_loss_{elapsed_time}_seconds.keras')
+                        f.write(f"* {acc_perc}, {data_perc}, {b}, {history.params}, {history.history['acc']}, {history.history['loss']}, {elapsed_time}\n")
                         continue
+                    elif acc >= 0.5:
+                        f.write(f"{acc_perc}, {data_perc}, {b}, {learning_rate}, {history.params}, {history.history['acc']}, {history.history['loss']}, {elapsed_time}\n")
+                    else:
+                        print(f"skipping write of model with acc:{acc_perc}")
+                        
+                    f.close() 
                     print(f"skipping model with accuracy: {acc_perc}")
+                    
+                    del history 
                 except Exception as ex:
                     # write out failure
                     f = open('failures.txt', 'a')
