a48e36e91689255892952232d09d30a40f5db757
jtrull101 <33765387+jtrull101@users.noreply.github.com>
2023-10-12 15:44:18 -0400

    Update create_kaggle_json.sh

--- create_kaggle_json.sh
+++ create_kaggle_json.sh
@@ -3,9 +3,11 @@ mkdir ~/.kaggle
 touch ~/.kaggle/kaggle.json
 echo '{"username":'\"$KAGGLE_USERNAME\"',"key":'\"$KAGGLE_KEY\"'}' > ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
+cat ~/.kaggle/kaggle.json
 
 # git hub actions
 mkdir /home/runner/.kaggle
 touch /home/runner/.kaggle/kaggle.json
 echo '{"username":'\"$KAGGLE_USERNAME\"',"key":'\"$KAGGLE_KEY\"'}' > /home/runner/.kaggle/kaggle.json
 chmod 600 /home/runner/.kaggle/kaggle.json
+cat /home/runner/.kaggle/kaggle.json
