28742d5bbb389335518e4d24cd872f01ee4074e6
jtrull101 <33765387+jtrull101@users.noreply.github.com>
2023-10-12 16:00:00 -0400

    editstotests.yml

--- create_kaggle_json.sh
+++ create_kaggle_json.sh
@@ -2,15 +2,14 @@
 username=$1
 key=$2
 
+# typical kaggle dir
 mkdir ~/.kaggle
 touch ~/.kaggle/kaggle.json
 echo '{"username":'\"$username\"',"key":'\"$key\"'}' > ~/.kaggle/kaggle.json
 chmod 600 ~/.kaggle/kaggle.json
-cat ~/.kaggle/kaggle.json
 
-# git hub actions
+# git hub actions dir
 mkdir /home/runner/.kaggle
 touch /home/runner/.kaggle/kaggle.json
 echo '{"username":'\"$username\"',"key":'\"$key\"'}' > /home/runner/.kaggle/kaggle.json
 chmod 600 /home/runner/.kaggle/kaggle.json
-cat /home/runner/.kaggle/kaggle.json
